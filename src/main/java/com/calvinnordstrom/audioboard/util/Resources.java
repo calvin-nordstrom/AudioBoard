026fa69f6e207a942c1d4f0f7deeae0cf016419b
Calvin Nordstrom <calnordstrom@icloud.com>
2026-06-11 23:36:03 -0400

    feat: add resource utility

---
+++ src/main/java/com/calvinnordstrom/audioboard/util/Resources.java
@@ -0,0 +1,31 @@
+package com.calvinnordstrom.audioboard.util;
+
+import com.calvinnordstrom.audioboard.Main;
+import javafx.scene.image.Image;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public final class Resources {
+    public static final String STYLES = getResource("css/styles.css");
+    public static final String DEFAULT_ICON = getResource("icons/default_icon.png");
+
+    private Resources() {
+    }
+
+    public static String getResource(String name) {
+        return Objects.requireNonNull(
+                Main.class.getResource(name),
+                () -> "Resource not found: " + name
+        ).toExternalForm();
+    }
+
+    public static Image getImage(Path path) {
+        if (path != null && Files.isRegularFile(path)) {
+            return new Image(path.toUri().toString());
+        }
+
+        return new Image(DEFAULT_ICON);
+    }
+}
